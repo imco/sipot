7c409ce533e24e5d7de0209efec05c4361c3a1f3
Carranza <fatcarranza@protonmail.com>
2020-04-12 21:55:01 -0700

    Rename scraper

--- cli.js
+++ cli.js
@@ -2,13 +2,13 @@
 // usage: cli.js --organization "Secretaría de Cultura"
 
 const argv = require('minimist')(process.argv.slice(2))
-const contracts = require('./contracts')
+const scraper = require('./scraper')
 
 const organization = argv.organization
 
 ;(async () => {
   try {
-    await contracts.getContract(organization)
+    await scraper.getContract(organization)
     console.log('Terminamos el scraping')
   } catch (e) {
     console.log('Algo falló')
