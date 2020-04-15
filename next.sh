4698f3b606c5a838b6eba211e72767e4f960a702
Carranza <fatcarranza@protonmail.com>
2020-04-15 11:29:06 -0700

    Find out what organizations are pending for the next scraping session

---
+++ next.sh
@@ -0,0 +1,11 @@
+# Build index of downloads
+./etl.js --directory 2018/ > 2018.csv
+./etl.js --directory 2019/ > 2019.csv
+
+# Get list of downloaded organizations
+cut -d ';' -f1 2018.csv | tail -n +2 | sort > 2018.downloaded
+cut -d ';' -f1 2019.csv | tail -n +2 | sort > 2019.downloaded
+
+# Find organizations with no downloads
+comm obligados.txt 2018.downloaded -23 > 2018.only
+comm obligados.txt 2019.downloaded -23 > 2019.only
