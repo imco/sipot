0f0f5b7414a2f8c3911110707207b4779bf06b5d
Carranza <fatcarranza@protonmail.com>
2020-04-18 17:12:06 -0700

    Unescape and unquote orgs for next round

--- next.sh
+++ next.sh
@@ -9,3 +9,7 @@ cut -d ';' -f1 2019.csv | tail -n +2 | sort > 2019.downloaded
 # Find organizations with no downloads
 comm obligados.txt 2018.downloaded -23 > 2018.only
 comm obligados.txt 2019.downloaded -23 > 2019.only
+
+# Unescape quotes
+sed 's/^"//' 2018.only | sed 's/"$//' | sed 's/\\"/"/g' > 2018.next
+sed 's/^"//' 2019.only | sed 's/"$//' | sed 's/\\"/"/g' > 2019.next
