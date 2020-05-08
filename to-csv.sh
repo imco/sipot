ec9d4247af7957330d554359cb63df62303f29d5
Carranza <fatcarranza@protonmail.com>
2020-05-07 19:53:29 -0700

    Update to-csv script so we don't get sporadic KeyError
    when parsing some xls files

--- to-csv.sh
+++ to-csv.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # usage: ./to-csv.sh [input] [cols-to-skip]
 
-# 1. Skips the first lines down to the start of the data table
+# 1. Adds default column names a,b,c,d,e
 # 2. Removes some columns
 # 3. Line breaks found within cells are transformed to spaces
 # 4. Outputs to stdout
+# 5. Skipping the first lines down to the start of the data table
 
-in2csv --skip-lines 6 --no-header-row $1 | \
+in2csv --no-header-row $1 | \
   csvcut --not-columns $2 | \
   csvformat -U 1 -M '@' | \
-  tr '\n@' ' \n'
+  tr '\n@' ' \n' | \
+  awk 'NR>7'
