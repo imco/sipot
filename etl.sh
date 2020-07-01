f6c99fecd3fb33993d54f10fbdf8a395440d63fd
Carranza <fatcarranza@protonmail.com>
2020-06-30 23:43:31 -0700

    Make etl.sh run other OP types

--- etl.sh
+++ etl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# usage: ./tablas.sh
-# Run a bunch of etl.js --op table commands
+# usage: ./etl.sh [merge|table]
+# Run a bunch of etl.js --op commands
 
 years=(2018 2019)
 types=(licitaciones adjudicaciones)
@@ -9,7 +9,7 @@ formats=(xls xlsx)
 for type in "${types[@]}"; do
   for year in "${years[@]}"; do
     for format in "${formats[@]}"; do
-      time ./etl.js --op table --cores 12 \
+      time ./etl.js --op "$1" --cores 12 \
         --directory data/"$type"/"$year" --format "$format" --type "$type"
     done
   done
